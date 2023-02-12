5e74024ef742f39b99c0b5adaa699579a708ae6b
takashikawanaka <takashi.soccer.a@gmail.com>
2023-02-12 16:31:36 +0900

    add Chapter9

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.techacademy.controller;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
@@ -33,6 +37,24 @@ public class UserController {
     @PostMapping("/register")
     public String postRegister(User user) {
         service.saveUser(user);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/update/{id}")
+    public String getUser(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("user", service.getUser(id));
+        return "user/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String postUser(User user) {
+        service.saveUser(user);
+        return "redirect:/user/list";
+    }
+
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        service.deleteUser(idck);
         return "redirect:/user/list";
     }
 
